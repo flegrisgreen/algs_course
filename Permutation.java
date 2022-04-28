cac51ead756859d6123e1f82ee3e382ad23faf5c
carlo.human <carlo.human@vastech.co.za>
2022-04-28 13:45:53 +0200

    permutation working

--- Permutation.java
+++ Permutation.java
@@ -4,8 +4,23 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        String input = "start";
+        RandomizedQueue<String> inputs = new RandomizedQueue<String>();
 
+        while (!StdIn.isEmpty()) {
+            input = StdIn.readString();
+            inputs.enqueue(input);
+        }
+
+        while (k > 0) {
+            StdOut.println(inputs.dequeue());
+            k--;
+        }
     }
 }
