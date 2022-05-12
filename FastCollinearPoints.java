a727b4698c9fa988a230389015eef09683a1ea50
carlo.human <carlo.human@vastech.co.za>
2022-05-12 16:42:45 +0200

    started fastcollinearpoints

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -5,6 +5,22 @@
  **************************************************************************** */
 
 public class FastCollinearPoints {
+
+    public FastCollinearPoints(Point[] points) {
+
+        // continue. Create loop to go through each point
+        // Loop to get slopes relative to point and compare adjacent
+    }
+
+    public int numberOfSegements() {
+        // continue
+        return 0;
+    }
+
+    public LineSegment[] segments() {
+        return null;
+    }
+
     public static void main(String[] args) {
 
     }
